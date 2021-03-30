e2a786a42114dd307dd1ee3f0ef5d440b8d7130c
Wenkai Yin <yinw@vmware.com>
2021-03-30 21:51:28 +0800

    update

--- src/io/goharbor/harbor/jenkins/library/HarborInstance.groovy
+++ src/io/goharbor/harbor/jenkins/library/HarborInstance.groovy
@@ -6,6 +6,7 @@ public class HarborInstance implements Serializable{
     public boolean trivyEnabled;
     public boolean notaryEnabled;
     public boolean chartmuseumEnabled;
+    public String systemAdminPassword;
     public Map ipHostnameMappings; // the mapping of IP address and hostname
     // TODO add certificates, so that the tester can trust them in the OS level?
 }
--- src/io/goharbor/harbor/jenkins/library/TestCaseRunner.groovy
+++ src/io/goharbor/harbor/jenkins/library/TestCaseRunner.groovy
@@ -15,11 +15,18 @@ class TestCaseRunner implements Serializable {
         script.checkout scm: [$class: "GitSCM", userRemoteConfigs: [[url: "https://github.com/goharbor/harbor.git"]], branches: [[name: "$testCaseRef"]],
             extensions: [[$class: "RelativeTargetDirectory", relativeTargetDir: "harbor"]]]
 
-        String coreServiceURL = instance.coreServiceURL
+        // TODO 1. set the image as parameter?
+        // TODO 2. support other options
+        // TODO 3. refine the image
+        // TODO 4. replace the "harbor-repo.vmware.com"?
+        // TODO 5. set the dockerhub credential
         script.sh """
             docker run -i --privileged --rm -w /drone -v \$(pwd)/harbor:/drone \
                 harbor-repo.vmware.com/harbor-ci/goharbor/harbor-e2e-engine:2.6.3 \
-                robot -v ip:$instance.coreServiceURL \
+                robot -v ip:$instance.coreServiceURL -v notaryServerEndpoint:$instance.notaryServiceURL \
+                    -v LOCAL_REGISTRY:harbor-repo.vmware.com -v LOCAL_REGISTRY_NAMESPACE:harbor-ci \
+                    -v DOCKER_USER:username -v DOCKER_PWD:password \
+                    -v HARBOR_PASSWORD:$instance.systemAdminPassword \
                     /drone/tests/robot-cases/Group1-Nightly/Setup_Nightly.robot
         """
     }
