f25e9875ee23ba820ac648f003d363857efccb67
Wenkai Yin <yinw@vmware.com>
2021-03-30 21:40:05 +0800

    update

--- src/io/goharbor/harbor/jenkins/library/TestCaseRunner.groovy
+++ src/io/goharbor/harbor/jenkins/library/TestCaseRunner.groovy
@@ -2,7 +2,7 @@ package io.goharbor.harbor.jenkins.library
 
 class TestCaseRunner implements Serializable {
     private Script script;
-    private String testCaseRef = "master"; // the branch/tag of test cases
+    private String testCaseRef = "master"; // the branch/tag of the harbor repository where the test cases are in
     private HarborInstance instance;
 
     TestCaseRunner(Script script, HarborInstance instance) {
@@ -12,14 +12,14 @@ class TestCaseRunner implements Serializable {
 
     void run(){
         // checkout the test case into the "harbor" folder
-        script.checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: 'https://github.com/goharbor/harbor.git']], branches: [[name: 'master']],
-            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'harbor']]]
+        script.checkout scm: [$class: "GitSCM", userRemoteConfigs: [[url: "https://github.com/goharbor/harbor.git"]], branches: [[name: "$testCaseRef"]],
+            extensions: [[$class: "RelativeTargetDirectory", relativeTargetDir: "harbor"]]]
 
-        String coreServiceURL = instance.coreServiceURL()
+        String coreServiceURL = instance.coreServiceURL
         script.sh """
-            docker run -i --privileged --rm -v \$(pwd)/harbor:/drone -w /drone \
+            docker run -i --privileged --rm -w /drone -v \$(pwd)/harbor:/drone \
                 harbor-repo.vmware.com/harbor-ci/goharbor/harbor-e2e-engine:2.6.3 \
-                robot -v ip:$coreServiceURL \
+                robot -v ip:$instance.coreServiceURL \
                     /drone/tests/robot-cases/Group1-Nightly/Setup_Nightly.robot
         """
     }
