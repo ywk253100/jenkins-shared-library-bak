8bb900762573d3d66455be7a9b0ad786d9aa4c61
Wenkai Yin <yinw@vmware.com>
2021-04-01 22:05:03 +0800

    update

--- src/io/goharbor/harbor/jenkins/library/HarborInstance.groovy
+++ src/io/goharbor/harbor/jenkins/library/HarborInstance.groovy
@@ -3,9 +3,6 @@ package io.goharbor.harbor.jenkins.library
 public class HarborInstance implements Serializable{
     public String coreServiceURL;
     public String notaryServiceURL;
-    public boolean trivyEnabled;
-    public boolean notaryEnabled;
-    public boolean chartmuseumEnabled;
     public String systemAdminPassword;
     public Map ipHostnameMappings; // the mapping of IP address and hostname
     // TODO add certificates, so that the tester can trust them in the OS level?
